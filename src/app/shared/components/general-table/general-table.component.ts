dabcfd8ebfa285ebfa3e8867427933c0d728b050
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-10-22 18:55:39 -0500

    feat: configuracion inicial para tabla general #3

---
+++ src/app/shared/components/general-table/general-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-general-table',
+  templateUrl: './general-table.component.html',
+  styleUrls: ['./general-table.component.scss']
+})
+export class GeneralTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import * as fromShared from './reducers/shared.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { SharedEffects } from './effects/shared.effects';
+import { GeneralTableComponent } from './components/general-table/general-table.component';
 
 @NgModule({
   exports: [
@@ -17,6 +18,7 @@ import { SharedEffects } from './effects/shared.effects';
     StoreModule.forFeature(fromShared.sharedFeatureKey, fromShared.reducer),
     EffectsModule.forFeature([SharedEffects])
   ],
+  declarations: [GeneralTableComponent],
 })
 
 export class SharedModule { }
