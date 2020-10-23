082d10b46e87bd07adad990b54a1d53feea33a45
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-10-23 14:54:53 -0500

    feat: ajustes iniciales para tabla dinamica #3

--- src/app/shared/components/general-table/general-table.component.ts
+++ src/app/shared/components/general-table/general-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CONFIGURACION_PRUEBA, DATOS_PRUEBA } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'ngx-general-table',
@@ -9,11 +10,19 @@ export class GeneralTableComponent implements OnInit {
 
   @Input() config: any;
   @Output() selectedAction: EventEmitter<any>;
+  stringBusqueda: string;
+  datosPrueba: any[];
 
   constructor() {
+    this.stringBusqueda = '';
+    this.selectedAction = new EventEmitter<any>();
+    this.config = CONFIGURACION_PRUEBA;
+    this.datosPrueba = DATOS_PRUEBA;
+    console.log(this.config, this.datosPrueba)
   }
 
   ngOnInit() {
+
   }
 
 }
