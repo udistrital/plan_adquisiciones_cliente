70bebfc0416726a2147fca84b2e731c6d922271e
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-10-23 13:08:40 -0500

    feat: creacion de datos de prueba para tabla #3

---
+++ src/app/shared/interfaces/interfaces.ts
@@ -0,0 +1,99 @@
+const configTable: any = {
+    title: {
+        name:'string',
+        class: 'string',
+    },
+    dataConfig: [
+        {
+            key: 'string',
+            title: 'string',
+            pipe: {
+                functionPipe: () => {},
+                class: 'string'
+            }
+        }
+    ],
+    rowActions: [
+        {
+            name: 'string',
+            icon: 'string',
+            class: 'string',
+            title: 'string',
+        }
+    ],
+    tableActions: [
+        {
+            name: 'string',
+            icon: 'string',
+            class: 'string',
+            title: 'string',
+        }
+    ],
+    sort: 'boolean',
+    filter: 'boolean',
+}
+export const CONFIGURACION_PRUEBA: any = {
+    title: {
+        name:'Tabla de Prueba',
+        class: 'text-center',
+    },
+    dataConfig: [
+        {
+            key: 'id',
+            title: 'Consecutivo',
+            pipe: {
+                functionPipe: (data: string) => {
+                    return data + ' ' + data;
+                },
+                class: 'text-uppercase'
+            }
+        },
+        {
+            key: 'nombre',
+            title: 'Nombre',
+            pipe: {
+                functionPipe: (data: string) => {
+                    return data + ' ' + data;
+                },
+                class: 'text-uppercase'
+            }
+        },
+        
+    ],
+    rowActions: [
+        {
+            name: 'string',
+            icon: 'string',
+            class: 'string',
+            title: 'string',
+        }
+    ],
+    tableActions: [
+        {
+            name: 'string',
+            icon: 'string',
+            class: 'string',
+            title: 'string',
+        }
+    ],
+    sort: 'boolean',
+    filter: true,
+}
+export const DATOS_PRUEBA : any = [
+    {
+        id: 1,
+        nombre: 'Jhoan',
+    },
+    {
+        id: 2,
+        nombre: 'Manuel',
+    },
+    {
+        id: 3,
+        nombre: 'Murillo',
+    },
+    {
+        id: 4,
+        nombre: 'Yara',
+    },
+]
