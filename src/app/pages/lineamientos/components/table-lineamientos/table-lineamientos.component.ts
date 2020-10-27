108fcd94f8af939b09086918ad7b0df459675bbe
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-10-26 20:45:14 -0500

    feat: creacion de tabla para lineamientos #3

---
+++ src/app/pages/lineamientos/components/table-lineamientos/table-lineamientos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-table-lineamientos',
+  templateUrl: './table-lineamientos.component.html',
+  styleUrls: ['./table-lineamientos.component.scss']
+})
+export class TableLineamientosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
