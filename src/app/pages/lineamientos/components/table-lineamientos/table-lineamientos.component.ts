4405e72b10ecfca77a2a560cb95d33597073bde0
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-10-26 23:10:46 -0500

    feat: ajuste vista lineamientos #3

--- src/app/pages/lineamientos/components/table-lineamientos/table-lineamientos.component.ts
+++ src/app/pages/lineamientos/components/table-lineamientos/table-lineamientos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CONFIGURACION_PRUEBA, DATOS_PRUEBA } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'ngx-table-lineamientos',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableLineamientosComponent implements OnInit {
 
-  constructor() { }
+  configuracion: any;
+  datosPrueba: any;
+  constructor() {
+    this.datosPrueba = DATOS_PRUEBA;
+    this.configuracion = CONFIGURACION_PRUEBA;
+  }
 
   ngOnInit() {
   }
