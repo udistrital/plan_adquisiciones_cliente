64518bb30bd3d9ac8469a4d1ecb781df3ea9c8dc
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-11-11 18:25:30 -0500

    feat: creacion de servicio para lineamientos

---
+++ src/app/pages/lineamientos/services/lineamientos.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { PopUpManager } from '../../../@core/managers/popUpManager';
+import { RequestManager } from '../../../@core/managers/requestManager';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LineamientosService {
+
+  constructor(private rqManager: RequestManager,
+    private pUpManager: PopUpManager) { }
+
+  /**
+   * Get Lineamientos
+   *  retorna los lineamientos asociados a la fuente de recurso, Centro Gestor y Area Funcional seleccionada
+   * @param [CentroGestor] Caentro Gestor Asociado
+   * @param [AreaFuncionalId] Area Funcional Asociado
+   * @param [FuenteRecursoId] Fuente de Recurso Asociado
+   * @returns  Lineamientos Asociados.
+   */
+  public getLineamientosAsociados(
+    CentroGestor: any,
+    AreaFuncionalId: any,
+    FuenteRecursoId: any
+    ) {
+    this.rqManager.setPath('PLAN_ADQUISICIONES_CRUD_SERVICE');
+    return this.rqManager.get(
+      `lineamiento/` +
+      `?query=Activo:true,` +
+      `CentroGestor:${CentroGestor},` +
+      `AreaFuncionalId:${AreaFuncionalId},` +
+      `FuenteRecursoId:${FuenteRecursoId}`
+    );
+  }
+  /**
+    * Post Lineamientos
+    *  se crea un lineamiento nuevo
+    * @param [Lineamiento] lineamiento por crear
+    * @returns  Lineamiento creado.
+    */
+  public crearLineamiento(
+    Lineamiento: any
+    ) {
+    this.rqManager.setPath('PLAN_ADQUISICIONES_CRUD_SERVICE');
+    return this.rqManager.post(
+      `lineamiento/`,
+      Lineamiento
+    );
+  }
+  /**
+    * Put Lineamientos
+    *  se modifica un lineamiento
+    * @param [Lineamiento] lineamiento por modificar
+    * @returns  Lineamiento creado.
+    */
+   public updateLineamiento(
+    Lineamiento: any
+    ) {
+    this.rqManager.setPath('PLAN_ADQUISICIONES_CRUD_SERVICE');
+    return this.rqManager.putParams(
+      `lineamiento/`,
+      Lineamiento
+    );
+  }
+
+
+}
