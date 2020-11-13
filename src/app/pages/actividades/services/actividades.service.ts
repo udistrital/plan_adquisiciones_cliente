e9623de46b3cdd128576829e5970b02b8f41b3a7
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-11-13 16:35:08 -0500

    feat: creacion de servicio de actividades #9

---
+++ src/app/pages/actividades/services/actividades.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { PopUpManager } from '../../../@core/managers/popUpManager';
+import { RequestManager } from '../../../@core/managers/requestManager';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActividadesService {
+
+  constructor(private rqManager: RequestManager,
+    private pUpManager: PopUpManager) { }
+
+  /**
+   * Get Actividades
+   *  retorna las Actividades asociados a la Meta seleccionada
+   * @param [Meta] Meta Asociada
+   * @returns  Actividades Asociadas.
+   */
+  public getActividadsAsociadas(
+    MetaId: any,
+    ) {
+    this.rqManager.setPath('PLAN_ADQUISICIONES_CRUD_SERVICE');
+    return this.rqManager.get(
+      `Actividad/` +
+      `?query=Activo:true,` +
+      `MetaId:${MetaId},`
+    );
+  }
+  /**
+    * Post Actividad
+    *  se crea una Actividad nueva
+    * @param [Actividad] Actividad por crear
+    * @returns  Actividad creada.
+    */
+  public crearActividad(
+    Actividad: any
+    ) {
+    this.rqManager.setPath('PLAN_ADQUISICIONES_CRUD_SERVICE');
+    return this.rqManager.post(
+      `Actividad/`,
+      Actividad
+    );
+  }
+  /**
+    * Put Actividad
+    *  se modifica un Actividad
+    * @param [Actividad] Actividad por modificar
+    * @returns  Actividad Modificada.
+    */
+   public updateActividad(
+    Actividad: any
+    ) {
+    this.rqManager.setPath('PLAN_ADQUISICIONES_CRUD_SERVICE');
+    return this.rqManager.put(
+      `Actividad/`,
+      Actividad,
+      Actividad.Id
+    );
+  }
+}
