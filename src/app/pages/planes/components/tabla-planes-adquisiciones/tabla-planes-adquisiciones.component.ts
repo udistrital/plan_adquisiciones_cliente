ef00d85b30a84bc3213b5bd9fe0842cecf4b0258
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-11-19 17:14:51 -0500

    feat: ajuste carga de planes #14

--- src/app/pages/planes/components/tabla-planes-adquisiciones/tabla-planes-adquisiciones.component.ts
+++ src/app/pages/planes/components/tabla-planes-adquisiciones/tabla-planes-adquisiciones.component.ts
@@ -3,8 +3,9 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { LoadAccionTabla, LoadFilaSeleccionada } from '../../../../shared/actions/shared.actions';
 import { getAccionTabla, getFilaSeleccionada } from '../../../../shared/selectors/shared.selectors';
-import { SeleccionarPlan } from '../../actions/planes.actions';
+import { CargarPlanes, SeleccionarPlan } from '../../actions/planes.actions';
 import { CONFIGURACION_PRUEBA, DATOS_PRUEBA } from '../../interfaces/interfaces';
+import { getPlanes } from '../../selectors/planes.selectors';
 
 @Component({
   selector: 'ngx-tabla-planes-adquisiciones',
@@ -17,21 +18,37 @@ export class TablaPlanesAdquisicionesComponent implements OnInit, OnDestroy {
   datosPrueba: any;
 
   subscription$: any;
+  subscription2$: any;
+  subscription3$: any;
 
   constructor(
     private store: Store<any>,
     private route: Router,
   ) {
     this.configuracion = CONFIGURACION_PRUEBA;
-    this.datosPrueba = DATOS_PRUEBA;
     this.store.dispatch(LoadAccionTabla(null));
+    this.store.dispatch(CargarPlanes([DATOS_PRUEBA]));
   }
   ngOnDestroy(): void {
     this.subscription$.unsubscribe();
   }
 
   ngOnInit() {
-    this.subscription$ = this.store.select(getAccionTabla).subscribe((accion) => {
+    // Cargar planes Asociados
+    this.subscription$ = this.store.select(getPlanes).subscribe((planes: any) => {
+      console.log(planes)
+      if (planes) {
+        if (Object.keys(planes).length !== 0) {
+          if (Object.keys(planes[0][0]).length !== 0) {
+            this.datosPrueba = planes[0];
+          } else {
+            this.datosPrueba = [];
+          }
+        }
+      }
+    });
+    // Seleccionar Nuevo Plan
+    this.subscription2$ = this.store.select(getAccionTabla).subscribe((accion) => {
       if (accion) {
         if (Object.keys(accion)[0] !== 'type') {
           this.store.dispatch(SeleccionarPlan(null));
@@ -39,7 +56,8 @@ export class TablaPlanesAdquisicionesComponent implements OnInit, OnDestroy {
         }
       }
     });
-    this.subscription$ = this.store.select(getFilaSeleccionada).subscribe((accion) => {
+    // Seleccionar Fila Tabla
+    this.subscription3$ = this.store.select(getFilaSeleccionada).subscribe((accion) => {
       if (accion) {
         if (Object.keys(accion)[0] !== 'type') {
           this.store.dispatch(SeleccionarPlan(accion.fila));
