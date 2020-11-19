181bed1bd23a975df38fe3ada05777cc2fc699dd
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-11-18 21:16:05 -0500

    feat: creacion de componentes para vista #14

---
+++ src/app/pages/planes/components/crear-plan-adquisicion/crear-plan-adquisicion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-crear-plan-adquisicion',
+  templateUrl: './crear-plan-adquisicion.component.html',
+  styleUrls: ['./crear-plan-adquisicion.component.scss']
+})
+export class CrearPlanAdquisicionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/planes/components/tabla-planes-adquisiciones/tabla-planes-adquisiciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-tabla-planes-adquisiciones',
+  templateUrl: './tabla-planes-adquisiciones.component.html',
+  styleUrls: ['./tabla-planes-adquisiciones.component.scss']
+})
+export class TablaPlanesAdquisicionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
