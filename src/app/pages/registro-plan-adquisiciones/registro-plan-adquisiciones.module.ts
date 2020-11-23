32f35c7a1001c19114d292e5ff82227a439755ed
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-11-23 17:19:30 -0500

    feat: creacion modulo registro renglon plan adquisiciones #13

---
+++ src/app/pages/registro-plan-adquisiciones/registro-plan-adquisiciones.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegistroPlanAdquisicionesRoutingModule } from './registro-plan-adquisiciones-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RegistroPlanAdquisicionesRoutingModule
+  ]
+})
+export class RegistroPlanAdquisicionesModule { }
