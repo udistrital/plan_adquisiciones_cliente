4e77d65ae2e79fa0cbe877c616109988b3af7d90
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-11-23 18:49:51 -0500

    feat: creacion componente seleccion meta y producto #13

---
+++ src/app/pages/registro-plan-adquisiciones/components/seleccion-meta-producto/seleccion-meta-producto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-seleccion-meta-producto',
+  templateUrl: './seleccion-meta-producto.component.html',
+  styleUrls: ['./seleccion-meta-producto.component.scss']
+})
+export class SeleccionMetaProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
