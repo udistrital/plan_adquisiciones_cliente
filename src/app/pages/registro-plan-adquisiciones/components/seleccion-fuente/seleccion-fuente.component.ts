60c99180780eaf5b3b73ea35e0a7162cb82d751b
Jhoan Manuel Murillo Yara <jmmurilloy@correo.udistrital.edu.co>
2020-11-23 18:50:27 -0500

    feat: creacion componente seleccion fuente financiamiento #13

---
+++ src/app/pages/registro-plan-adquisiciones/components/seleccion-fuente/seleccion-fuente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-seleccion-fuente',
+  templateUrl: './seleccion-fuente.component.html',
+  styleUrls: ['./seleccion-fuente.component.scss']
+})
+export class SeleccionFuenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
