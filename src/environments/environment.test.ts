6dde32d158571af1efb8098833537ef45554ff80
brayanpasa99 <brayanpasa99@hotmail.com>
2022-05-20 14:03:51 -0500

    devops: Ajustes a los ambientes
    Resuelve: udistrital/plan_adquisiciones_cliente#117

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -8,42 +8,13 @@ import { SHARED } from './shared';
 export const environment = {
   ...SHARED,
 
-  production: false,
-  NUXEO: {
-    PATH: 'https://documental.udistrital.edu.co/nuxeo/',
-  },
-  CLIENTE_PRESUPUESTO: '/pages/plan-cuentas',
-  CLIENTE_CONTABILIDAD:
-    'https://pruebascontabilidad.portaloas.udistrital.edu.co/pages',
-  WSO2_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/',
+  production: true,
   // PLAN_ADQUISICIONES_CRUD_SERVICE: 'http://localhost:8081/v1/',
-  PLAN_ADQUISICIONES_CRUD_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_adquisiciones_crud/v1/',
-  PLAN_ADQUISICIONES_MID_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_adquisiciones_mid/v1/',
   // PLAN_ADQUISICIONES_MID_SERVICE: 'http://localhost:8083/v1/',
-  CATALOGO_ELEMENTOS_ARKA_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/catalogo_elementos_crud/v1/',
   // ADMINISTRATIVA_PRUEBAS_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8104/v1/',
-  PLAN_CUENTAS_MONGO_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_cuentas_mongo_crud/v1/',
   // PLAN_CUENTAS_MONGO_SERVICE: 'http://localhost:8082/v1/',
-  PLAN_CUENTAS_MID_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_cuentas_mid/v1/',
   // PLAN_CUENTAS_MID_SERVICE: 'http://localhost:8080/v1/',
   // PLAN_CUENTAS_MID_SERVICE: 'http://pruebasapi.intranetoas.udistrital.edu.co:8204/v1/',
-  ADMINISTRATIVA_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_crud_api/v1/',
-  NECESIDADES_CRUD_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/necesidades_crud/v1/',
-  OIKOS_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1/',
-  OIKOS_2_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
-  CONFIGURACION_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
-  CONF_MENU_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
   IDPLANADQUISICIONES: '625e10947e642dc1653e121b',
   IDPLANADQUISICIONESIDEXUD: '61ef760a6d6ad467ee92e2aa',
   KNOWAGE: {
@@ -55,14 +26,12 @@ export const environment = {
     PASSWORD: 'bidev',
   },
   TOKEN: {
-    AUTORIZATION_URL:
-      'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
-    CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
+    AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
+    CLIENTE_ID: 'xytbuSBwcNJv9kLh57_zcfeN0REa',
     RESPONSE_TYPE: 'id_token token',
     SCOPE: 'openid email role',
-    REDIRECT_URL: 'http://localhost:4200/',
-    SIGN_OUT_URL:
-      'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
-    SIGN_OUT_REDIRECT_URL: 'http://localhost:4200/',
+    REDIRECT_URL: 'https://pruebasplanadquisiciones.portaloas.udistrital.edu.co',
+    SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
+    SIGN_OUT_REDIRECT_URL: 'https://pruebasplanadquisiciones.portaloas.udistrital.edu.co',
   },
 };
